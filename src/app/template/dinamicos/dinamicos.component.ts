63d4ee1eaf8c7c32e660fba020086768c9e6e472
Ainhoa Lopez <ainhoalv92@gmail.com>
2022-08-21 13:17:50 +0200

    Formulario: template -  dinámicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+
+  nombre:    string;
+  favoritos: Favorito[];
+
+}
+
+interface Favorito {
+
+  id:     number;
+  nombre: string;
+
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +21,59 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  @ViewChild ('miFormulario') miFormulario!: NgForm;
 
-  ngOnInit(): void {
+  nuevoJuego: string = '';
+
+  persona: Persona = {
+
+    nombre: 'Ainhoa',
+    favoritos: [
+
+      {id: 1, nombre: 'Zelda: Breath of the Wild'},
+      {id: 2, nombre: 'Hollow Knight'},
+      {id: 3, nombre: 'Death Stranding'}
+
+    ]
+  }
+
+  nombreInvalido(): any {
+
+    return (
+
+      this.miFormulario?.form.controls['nombre']?.errors &&
+      this.miFormulario?.form.controls['nombre']?.touched
+    
+    )
+
+  }
+
+  agregarJuego() {
+
+    const nuevoFavorito: Favorito = {
+      
+      id:     this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+
+    };
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
+
+  }
+
+  eliminar( index: number ) {
+
+    this.persona.favoritos.splice(index, 1)
+    
+  }
+
+  guardar() {
+
+    console.log('Formulario posteado');
+    
   }
 
 }
